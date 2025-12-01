61cd2201831bb43ef18f34fb92a01eb133fa6160
Ivan Trepakov <liontiger23@gmail.com>
2025-12-01 12:27:16 +0700

    puzzle1: solve part 1

--- src/Puzzle1.hs
+++ src/Puzzle1.hs
@@ -7,5 +7,20 @@ module Puzzle1
 import Util
 
 puzzle1 :: Int -> Solution Int
-puzzle1 1 = undefined
+puzzle1 1 = length . filter (== 0) . scanl (rotate 100) 50 . map distance
 puzzle1 2 = undefined
+
+-- >>> rotate 100 50 (-68)
+-- 82
+-- >>> rotate 100 82 (-30)
+-- 52
+-- >>> rotate 100 52 (48)
+-- 0
+-- >>> scanl (rotate 100) 50 [-68, -30, 48]
+-- [50,82,52,0]
+rotate :: Int -> Int -> Int -> Int
+rotate size x y = (x + y) `mod` size
+
+distance :: String -> Int
+distance ('L' : num) = - (read num)
+distance ('R' : num) =    read num
