248bd309d3ffc08a7d23e02949bdbd7d236dc6f1
Ivan Trepakov <liontiger23@gmail.com>
2025-12-02 12:33:15 +0700

    puzzle2: solve part 2

--- src/Puzzle2.hs
+++ src/Puzzle2.hs
@@ -7,21 +7,36 @@ module Puzzle2
 import Util
 import Data.List.Split (splitOn)
 import Data.Maybe (fromJust)
+import Data.List (nub)
 
 puzzle2 :: Int -> Solution Int
 puzzle2 1 = sum . concatMap (invalidIn . parseRange) . splitOn "," . fromJust . safeHead
-puzzle2 2 = undefined
+puzzle2 2 = sum . concatMap (invalidByNIn . parseRange) . splitOn "," . fromJust . safeHead
 
 invalidIn :: (Int, Int) -> [Int]
 invalidIn (l, r) = takeWhile (<= r) $ dropWhile (< l) invalid
 
 invalid :: [Int]
-invalid = go 1
+invalid = invalidN 2
+
+invalidN :: Int -> [Int]
+invalidN n = go 1
   where
     go :: Int -> [Int]
-    go part = read (show part <> show part) : go (part + 1)
+    go part = read (concat $ replicate n (show part)) : go (part + 1)
 
 parseRange :: String -> (Int, Int)
 parseRange str = case map read (splitOn "-" str) of
   [start, end] -> (start, end)
   xs -> error $ show xs
+
+invalidByNIn :: (Int, Int) -> [Int]
+invalidByNIn (l, r) = nub $ concatMap (takeWhile (<= r) . dropWhile (< l)) $
+  take (length $ show r) invalidByN
+
+-- >>> map (take 5) $ take 5 invalidByN 
+-- [[11,22,33,44,55],[111,222,333,444,555],[1111,2222,3333,4444,5555],[11111,22222,33333,44444,55555],[111111,222222,333333,444444,555555]]
+--
+invalidByN :: [[Int]]
+invalidByN = map invalidN [2..]
+
