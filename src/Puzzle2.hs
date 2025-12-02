3064fe2218b3d35900757d7f484857c8d3c88060
Ivan Trepakov <liontiger23@gmail.com>
2025-12-02 12:21:03 +0700

    puzzle2: solve part 1

--- src/Puzzle2.hs
+++ src/Puzzle2.hs
@@ -5,8 +5,23 @@ module Puzzle2
     ) where
 
 import Util
+import Data.List.Split (splitOn)
+import Data.Maybe (fromJust)
 
 puzzle2 :: Int -> Solution Int
-puzzle2 1 = undefined
+puzzle2 1 = sum . concatMap (invalidIn . parseRange) . splitOn "," . fromJust . safeHead
 puzzle2 2 = undefined
 
+invalidIn :: (Int, Int) -> [Int]
+invalidIn (l, r) = takeWhile (<= r) $ dropWhile (< l) invalid
+
+invalid :: [Int]
+invalid = go 1
+  where
+    go :: Int -> [Int]
+    go part = read (show part <> show part) : go (part + 1)
+
+parseRange :: String -> (Int, Int)
+parseRange str = case map read (splitOn "-" str) of
+  [start, end] -> (start, end)
+  xs -> error $ show xs
