6f033e8fba057c8aebe452b8327d35f93deb5f2a
Ivan Trepakov <liontiger23@gmail.com>
2025-12-04 23:13:40 +0700

    puzzle4: solve part 1

--- src/Puzzle4.hs
+++ src/Puzzle4.hs
@@ -5,7 +5,39 @@ module Puzzle4
     ) where
 
 import Util
+import Data.Map ((!))
+import qualified Data.Map as M
+import Data.Maybe (maybeToList)
+import Control.Monad.State
 
 puzzle4 :: Int -> Solution Int
-puzzle4 1 = undefined
+puzzle4 1 = length . evalState detectAccessible . parseInput
 puzzle4 2 = undefined
+
+detectAccessible :: State Map [Coordinate]
+detectAccessible = do
+  m <- get
+  let accessible c =
+        m ! c == '@' &&
+        length (filter (== '@') $ neighbours m c) < 4
+  pure $ filter accessible $ M.keys m
+
+neighbours :: Map -> Coordinate -> [Char]
+neighbours m (x, y) =
+  [ c
+  | x' <- [x - 1..x + 1]
+  , y' <- [y - 1..y + 1]
+  , (x, y) /= (x', y')
+  , c <- maybeToList $ M.lookup (x', y') m
+  ]
+
+parseInput :: [String] -> Map
+parseInput input = M.fromList
+  [ ((x, y), c)
+  | (x, l) <- zip [0..] input
+  , (y, c) <- zip [0..] l
+  ]
+
+type Map = M.Map Coordinate Char
+
+type Coordinate = (Int, Int)
