afbf8c7cb4a22a75ba3e7caedba3be9b04ca445c
Ivan Trepakov <liontiger23@gmail.com>
2025-12-04 13:04:56 +0700

    puzzle3: simplify part 1 (prepare for part 2)

--- src/Puzzle3.hs
+++ src/Puzzle3.hs
@@ -5,27 +5,17 @@ module Puzzle3
     ) where
 
 import Util
-import Data.Map (Map)
-import qualified Data.Map as Map
 
 puzzle3 :: Int -> Solution Int
-puzzle3 1 = sum . map maxJoltage
+puzzle3 1 = sum . map (maxJoltage 2)
 puzzle3 2 = undefined
 
-maxJoltage :: [Char] -> Int
-maxJoltage = maximum . map (read . pairToList) . Map.toDescList . Map.map maximum . Map.filter (not . null) . collectLeft
+maxJoltage :: Int -> [Char] -> Int
+maxJoltage n = read . maximum . splits n
 
--- reverse needed because fromList keeps the last duplicate key entry instead of the first
--- >>> collectLeft "123123"
--- fromList [('1',"23123"),('2',"3123"),('3',"123")]
-collectLeft :: [Char] -> Map Char [Char]
-collectLeft = Map.fromList . reverse . splits
-
--- >>> splits "123123"
--- [('1',"23123"),('2',"3123"),('3',"123"),('1',"23"),('2',"3"),('3',"")]
-splits :: [Char] -> [(Char, [Char])]
-splits [] = []
-splits (x : xs) = (x, xs) : splits xs
-
-pairToList :: (a, a) -> [a]
-pairToList (x, y) = [x, y]
+-- >>> splits 2 "123123"
+-- ["12","23","31","12","23"]
+splits :: Int -> [Char] -> [[Char]]
+splits _ [] = []
+splits 1 (x : xs) = [x] : splits 1 xs
+splits n (x : xs) = map (x :) (splits (n - 1) xs) <> splits n xs
