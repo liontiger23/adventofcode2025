411580353e2e4133478bc7040bbd33ef9ae9456b
Ivan Trepakov <liontiger23@gmail.com>
2025-12-06 14:33:21 +0700

    puzzle5: init

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,7 @@ import Puzzle1
 import Puzzle2
 import Puzzle3
 import Puzzle4
---import Puzzle5
+import Puzzle5
 --import Puzzle6
 --import Puzzle7
 --import Puzzle8
@@ -26,7 +26,7 @@ solve = do
     [Just 2, Just p] -> process (puzzle2 p)
     [Just 3, Just p] -> process (puzzle3 p)
     [Just 4, Just p] -> process (puzzle4 p)
---    [Just 5, Just p] -> process (puzzle5 p)
+    [Just 5, Just p] -> process (puzzle5 p)
 --    [Just 6, Just p] -> process (puzzle6 p)
 --    [Just 7, Just p] -> process (puzzle7 p)
 --    [Just 8, Just p] -> process (puzzle8 p)
---
+++ src/Puzzle5.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Puzzle5
+    ( puzzle5
+    ) where
+
+import Util
+
+puzzle5 :: Int -> Solution Int
+puzzle5 1 = undefined
+puzzle5 2 = undefined
