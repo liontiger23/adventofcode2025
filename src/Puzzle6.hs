6cf79e914bf5d54c0d81e2a2b38c11e543d84a51
Ivan Trepakov <liontiger23@gmail.com>
2025-12-06 18:52:37 +0700

    puzzle6: solve part 2

--- src/Puzzle6.hs
+++ src/Puzzle6.hs
@@ -8,10 +8,11 @@ import Util
 import Data.Char (isSpace)
 import Data.List (uncons)
 import Data.Maybe (fromJust)
+import Data.List.Split (splitOn)
 
 puzzle6 :: Int -> Solution Int
 puzzle6 1 = sum . map calc . parseInput . reverse . map skipSpaces
-puzzle6 2 = undefined
+puzzle6 2 = sum . map (calc . parseEquation2) . splitOn [[]] . map skipSpaces . transpose
 
 parseInput :: [[String]] -> [Equation]
 parseInput ([] : _) = []
@@ -30,6 +31,8 @@ data Equation = Add [Int] | Mul [Int]
 
 -- >>> skipSpaces "   123 3241  234 "
 -- ["123","3241","234"]
+-- >>> skipSpaces "     "
+-- []
 skipSpaces :: String -> [String]
 skipSpaces = go ""
  where
@@ -38,3 +41,14 @@ skipSpaces = go ""
   go acc (x : xs)
     | isSpace x = if null acc then go "" xs else acc : go "" xs
     | otherwise = go (acc ++ [x]) xs
+
+parseEquation2 :: [[String]] -> Equation
+parseEquation2 (argOp : args) =  case op of
+  '+' -> Add $ map read (arg : concat args)
+  '*' -> Mul $ map read (arg : concat args)
+ where
+  -- this looks weird, but it is needed to handle cases
+  -- where argOp is either ["123", "+"] or ["123+"]
+  (op, arg) = case reverse (concat argOp) of
+    (op' : revArg) -> (op', reverse revArg)
+parseEquation2 xs = error $ show xs
