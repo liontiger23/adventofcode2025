2debdcc41845428d3bb5f9f73977a37807474ac6
Ivan Trepakov <liontiger23@gmail.com>
2025-12-13 05:16:48 +0700

    puzzle9: fix x-y in rect

--- src/Puzzle9.hs
+++ src/Puzzle9.hs
@@ -15,9 +15,9 @@ allRects :: [Coordinate] -> [Rect]
 allRects cs = [rect a b | a <- cs, b <- cs]
 
 -- >>> rect (2,5) (9,7)
--- Rect 9 7 2 5
+-- Rect 7 9 5 2
 rect :: Coordinate -> Coordinate -> Rect
-rect (x1, y1) (x2, y2) = Rect (x1 `max` x2) (y1 `max` y2) (x1 `min` x2) (y1 `min` y2)
+rect (x1, y1) (x2, y2) = Rect (y1 `max` y2) (x1 `max` x2) (y1 `min` y2) (x1 `min` x2)
 
 data Rect = Rect Int Int Int Int
   deriving Show
@@ -25,7 +25,7 @@ data Rect = Rect Int Int Int Int
 -- >>> area $ rect (2,5) (9,7)
 -- 24
 area :: Rect -> Int
-area (Rect n e s w) = (abs (n - s) + 1) * (abs (w - e) + 1)
+area (Rect n e s w) = (abs (n - s) + 1) * (abs (e - w) + 1)
 
 parseCoordinate :: String -> Coordinate
 parseCoordinate input = case map read (splitOn "," input) of [x, y] -> (x, y)
