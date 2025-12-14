d3b3ecb0068f13cb512f542f148c74a006cfb5c3
Ivan Trepakov <liontiger23@gmail.com>
2025-12-14 16:34:03 +0700

    puzzle10: init

---
+++ src/Puzzle10.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Puzzle10
+    ( puzzle10
+    ) where
+
+import Util
+
+puzzle10 :: Int -> Solution Int
+puzzle10 1 = undefined
+puzzle10 2 = undefined
