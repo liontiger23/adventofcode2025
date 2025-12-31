5528646d6904f3aa6ded090bd048c91e645ad2ee
Ivan Trepakov <liontiger23@gmail.com>
2025-12-31 13:22:29 +0700

    puzzle11: init

---
+++ src/Puzzle11.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Puzzle11
+    ( puzzle11
+    ) where
+
+import Util
+
+puzzle11 :: Int -> Solution Int
+puzzle11 1 = undefined
+puzzle11 2 = undefined
