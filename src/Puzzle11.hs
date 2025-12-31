664743730edf7d9404ea34e4b5e396ede9bf7cbc
Ivan Trepakov <liontiger23@gmail.com>
2025-12-31 14:52:54 +0700

    puzzle11: solve part 2

--- src/Puzzle11.hs
+++ src/Puzzle11.hs
@@ -1,6 +1,4 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE DeriveFoldable #-}
 
 module Puzzle11
     ( puzzle11
@@ -14,11 +12,10 @@ import Data.Maybe (fromJust)
 
 puzzle11 :: Int -> Solution Int
 puzzle11 1 = length . pathsFromRoot "out" . buildGraph "you" . parseInput
-puzzle11 2 = undefined
+puzzle11 2 = solve2 . parseInput
 
 data Node a = Node a [Node a]
-  deriving (Show, Eq, Functor, Foldable)
-
+  deriving (Show, Eq)
 
 parseInput :: [String] -> M.Map String [String]
 parseInput = M.fromList . (("out", []) :) . map (fromJust . uncons . words . filter (/= ':'))
@@ -32,3 +29,14 @@ pathsFromRoot :: Eq a => a -> Node a -> [[a]]
 pathsFromRoot to (Node from outs)
   | to == from = [[from]]
   | otherwise  = concatMap (map (from :) . filter (not . null) . pathsFromRoot to) outs
+
+solve2 :: M.Map String [String] -> Int
+solve2 m 
+  | paths ! ("fft", "dac") > 0 = paths ! ("svr", "fft") * paths ! ("fft", "dac") * paths ! ("dac", "out")
+  | otherwise                  = paths ! ("svr", "dac") * paths ! ("dac", "fft") * paths ! ("fft", "out")
+ where
+  paths :: M.Map (String, String) Int
+  paths = M.fromList [((from, to), pathsNum from to) | from <- M.keys m, to <- M.keys m]
+  pathsNum from to
+    | from == to = 1
+    | otherwise  = sum (map (\x -> paths ! (x, to)) (m ! from))
